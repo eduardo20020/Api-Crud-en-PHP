73f924021974a0929251764091e86838b2c6a7b4
Brandon <brandonde2002@gmail.com>
2024-08-07 08:53:14 -0600

    sfdsfdsf

--- api/secciones.php
+++ api/secciones.php
@@ -6,37 +6,37 @@ $data = [
         [
             "nombre" => "seccion1",
             "subtemas" => [
-                ["subtema" => "subtema1", "contenido" => "Contenido del subtema 1 de la sección 1"],
-                ["subtema" => "subtema2", "contenido" => "Contenido del subtema 2 de la sección 1"],
-                ["subtema" => "subtema3", "contenido" => "Contenido del subtema 3 de la sección 1"],
-                ["subtema" => "subtema4", "contenido" => "Contenido del subtema 4 de la sección 1"]
+                ["subtema" => "subtema1", "contenido" => "Contenido del subtema 1 de la seccion 1"],
+                ["subtema" => "subtema2", "contenido" => "Contenido del subtema 2 de la seccion 1"],
+                ["subtema" => "subtema3", "contenido" => "Contenido del subtema 3 de la seccion 1"],
+                ["subtema" => "subtema4", "contenido" => "Contenido del subtema 4 de la seccion 1"]
             ]
         ],
         [
             "nombre" => "seccion2",
             "subtemas" => [
-                ["subtema" => "subtema1", "contenido" => "Contenido del subtema 1 de la sección 2"],
-                ["subtema" => "subtema2", "contenido" => "Contenido del subtema 2 de la sección 2"],
-                ["subtema" => "subtema3", "contenido" => "Contenido del subtema 3 de la sección 2"],
-                ["subtema" => "subtema4", "contenido" => "Contenido del subtema 4 de la sección 2"]
+                ["subtema" => "subtema1", "contenido" => "Contenido del subtema 1 de la seccion 2"],
+                ["subtema" => "subtema2", "contenido" => "Contenido del subtema 2 de la seccion 2"],
+                ["subtema" => "subtema3", "contenido" => "Contenido del subtema 3 de la seccion 2"],
+                ["subtema" => "subtema4", "contenido" => "Contenido del subtema 4 de la seccion 2"]
             ]
         ],
         [
             "nombre" => "seccion3",
             "subtemas" => [
-                ["subtema" => "subtema1", "contenido" => "Contenido del subtema 1 de la sección 3"],
-                ["subtema" => "subtema2", "contenido" => "Contenido del subtema 2 de la sección 3"],
-                ["subtema" => "subtema3", "contenido" => "Contenido del subtema 3 de la sección 3"],
-                ["subtema" => "subtema4", "contenido" => "Contenido del subtema 4 de la sección 3"]
+                ["subtema" => "subtema1", "contenido" => "Contenido del subtema 1 de la seccion 3"],
+                ["subtema" => "subtema2", "contenido" => "Contenido del subtema 2 de la seccion 3"],
+                ["subtema" => "subtema3", "contenido" => "Contenido del subtema 3 de la seccion 3"],
+                ["subtema" => "subtema4", "contenido" => "Contenido del subtema 4 de la seccion 3"]
             ]
         ],
         [
             "nombre" => "seccion4",
             "subtemas" => [
-                ["subtema" => "subtema1", "contenido" => "Contenido del subtema 1 de la sección 4"],
-                ["subtema" => "subtema2", "contenido" => "Contenido del subtema 2 de la sección 4"],
-                ["subtema" => "subtema3", "contenido" => "Contenido del subtema 3 de la sección 4"],
-                ["subtema" => "subtema4", "contenido" => "Contenido del subtema 4 de la sección 4"]
+                ["subtema" => "subtema1", "contenido" => "Contenido del subtema 1 de la seccion 4"],
+                ["subtema" => "subtema2", "contenido" => "Contenido del subtema 2 de la seccion 4"],
+                ["subtema" => "subtema3", "contenido" => "Contenido del subtema 3 de la seccion 4"],
+                ["subtema" => "subtema4", "contenido" => "Contenido del subtema 4 de la seccion 4"]
             ]
         ]
     ]
