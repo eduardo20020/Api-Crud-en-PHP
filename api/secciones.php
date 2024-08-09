2d7a97c1fc8e6afe6474c68a0e9c06f51ec288f1
Brandon <brandonde2002@gmail.com>
2024-08-08 21:47:03 -0600

    sfdsfdsf

--- api/secciones.php
+++ api/secciones.php
@@ -4,37 +4,37 @@ $data = [
     [
         "nombre" => "seccion1",
         "subtemas" => [
-            "sub1" => "aqui va el contenido del subtema1 dxdxd lorem20",
-            "sub2" => "aqui va el contenido del subtema1 dxdxd",
-            "sub3" => "aqui va el contenido del subtema1 dxdxd",
-            "sub4" => "aqui va el contenido del subtema1 dxdxd"
+            "aqui va el contenido del subtema1 dxdxd lorem20",
+            "aqui va el contenido del subtema1 dxdxd",
+            "aqui va el contenido del subtema1 dxdxd",
+            "aqui va el contenido del subtema1 dxdxd"
         ]
     ],
     [
         "nombre" => "seccion2",
         "subtemas" => [
-            "sub1" => "aqui va el contenido del subtema2 dxdxd",
-            "sub2" => "aqui va el contenido del subtema2 dxdxd",
-            "sub3" => "aqui va el contenido del subtema2 dxdxd",
-            "sub4" => "aqui va el contenido del subtema2 dxdxd"
+            "aqui va el contenido del subtema2 dxdxd",
+            "aqui va el contenido del subtema2 dxdxd",
+            "aqui va el contenido del subtema2 dxdxd",
+            "aqui va el contenido del subtema2 dxdxd"
         ]
     ],
     [
         "nombre" => "seccion3",
         "subtemas" => [
-            "sub1" => "aqui va el contenido del subtema3 dxdxd",
-            "sub2" => "aqui va el contenido del subtema3 dxdxd",
-            "sub3" => "aqui va el contenido del subtema3 dxdxd",
-            "sub4" => "aqui va el contenido del subtema3 dxdxd"
+            "aqui va el contenido del subtema3 dxdxd",
+            "aqui va el contenido del subtema3 dxdxd",
+            "aqui va el contenido del subtema3 dxdxd",
+            "aqui va el contenido del subtema3 dxdxd"
         ]
     ],
     [
         "nombre" => "seccion4",
         "subtemas" => [
-            "sub1" => "aqui va el contenido del subtema4 dxdxd",
-            "sub2" => "aqui va el contenido del subtema4 dxdxd",
-            "sub3" => "aqui va el contenido del subtema4 dxdxd",
-            "sub4" => "aqui va el contenido del subtema4 dxdxd"
+            "aqui va el contenido del subtema4 dxdxd",
+            "aqui va el contenido del subtema4 dxdxd",
+            "aqui va el contenido del subtema4 dxdxd",
+            "aqui va el contenido del subtema4 dxdxd"
         ]
     ]
 ];
