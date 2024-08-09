4f305c452972c0e418dd0688c1c5fd8b96a92cd2
Brandon <brandonde2002@gmail.com>
2024-08-08 20:59:34 -0600

    sfdsfdsf

--- api/secciones.php
+++ api/secciones.php
@@ -1,50 +1,47 @@
 <?php
-// Definir el arreglo de datos
+// Definir el array de datos
 $data = [
-    "num" => 4,
-    "secciones" => [
-        [
-            "nombre" => "seccion1",
-            "subtemas" => [
-                ["subtema" => "subtema1", "contenido" => "Contenido del subtema 1 de la seccion 1"],
-                ["subtema" => "subtema2", "contenido" => "Contenido del subtema 2 de la seccion 1"],
-                ["subtema" => "subtema3", "contenido" => "Contenido del subtema 3 de la seccion 1"],
-                ["subtema" => "subtema4", "contenido" => "Contenido del subtema 4 de la seccion 1"]
-            ]
-        ],
-        [
-            "nombre" => "seccion2",
-            "subtemas" => [
-                ["subtema" => "subtema1", "contenido" => "Contenido del subtema 1 de la seccion 2"],
-                ["subtema" => "subtema2", "contenido" => "Contenido del subtema 2 de la seccion 2"],
-                ["subtema" => "subtema3", "contenido" => "Contenido del subtema 3 de la seccion 2"],
-                ["subtema" => "subtema4", "contenido" => "Contenido del subtema 4 de la seccion 2"]
-            ]
-        ],
-        [
-            "nombre" => "seccion3",
-            "subtemas" => [
-                ["subtema" => "subtema1", "contenido" => "Contenido del subtema 1 de la seccion 3"],
-                ["subtema" => "subtema2", "contenido" => "Contenido del subtema 2 de la seccion 3"],
-                ["subtema" => "subtema3", "contenido" => "Contenido del subtema 3 de la seccion 3"],
-                ["subtema" => "subtema4", "contenido" => "Contenido del subtema 4 de la seccion 3"]
-            ]
-        ],
-        [
-            "nombre" => "seccion4",
-            "subtemas" => [
-                ["subtema" => "subtema1", "contenido" => "Contenido del subtema 1 de la seccion 4"],
-                ["subtema" => "subtema2", "contenido" => "Contenido del subtema 2 de la seccion 4"],
-                ["subtema" => "subtema3", "contenido" => "Contenido del subtema 3 de la seccion 4"],
-                ["subtema" => "subtema4", "contenido" => "Contenido del subtema 4 de la seccion 4"]
-            ]
+    [
+        "nombre" => "seccion1",
+        "subtemas" => [
+            "sub1" => "aqui va el contenido del subtema1 dxdxd lorem20",
+            "sub2" => "aqui va el contenido del subtema1 dxdxd",
+            "sub3" => "aqui va el contenido del subtema1 dxdxd",
+            "sub4" => "aqui va el contenido del subtema1 dxdxd"
+        ]
+    ],
+    [
+        "nombre" => "seccion2",
+        "subtemas" => [
+            "sub1" => "aqui va el contenido del subtema2 dxdxd",
+            "sub2" => "aqui va el contenido del subtema2 dxdxd",
+            "sub3" => "aqui va el contenido del subtema2 dxdxd",
+            "sub4" => "aqui va el contenido del subtema2 dxdxd"
+        ]
+    ],
+    [
+        "nombre" => "seccion3",
+        "subtemas" => [
+            "sub1" => "aqui va el contenido del subtema3 dxdxd",
+            "sub2" => "aqui va el contenido del subtema3 dxdxd",
+            "sub3" => "aqui va el contenido del subtema3 dxdxd",
+            "sub4" => "aqui va el contenido del subtema3 dxdxd"
+        ]
+    ],
+    [
+        "nombre" => "seccion4",
+        "subtemas" => [
+            "sub1" => "aqui va el contenido del subtema4 dxdxd",
+            "sub2" => "aqui va el contenido del subtema4 dxdxd",
+            "sub3" => "aqui va el contenido del subtema4 dxdxd",
+            "sub4" => "aqui va el contenido del subtema4 dxdxd"
         ]
     ]
 ];
 
-// Configurar el encabezado Content-Type a JSON
+// Establecer el tipo de contenido a JSON
 header('Content-Type: application/json');
 
-// Convertir el arreglo a JSON y enviarlo como respuesta
+// Enviar la respuesta JSON
 echo json_encode($data);
 ?>
