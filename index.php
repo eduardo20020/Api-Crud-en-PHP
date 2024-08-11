caa2896500a35dc8be1a4127871ca47fd1d21240
Brandon <brandonde2002@gmail.com>
2024-08-11 10:10:18 -0600

    sfdsfdsf

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="bg-light">
-    <div class="container mt-5">
+    <div class="container mt-3">
         <div class="row">
             <div class="col-md-5">
                 <!-- Aquí iría tu formulario para insertar o editar datos -->
