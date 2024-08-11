30a01be0096d159081411ef661348c3bba23f85e
Brandon <brandonde2002@gmail.com>
2024-08-11 10:08:18 -0600

    sfdsfdsf

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="bg-light">
-    <div class="container mt-5">
+    <div class="container mt-3">
         <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-5">
                 <!-- Aquí iría tu formulario para insertar o editar datos -->
                 <form action="api/ingresarleccion.php" method="post" class="card p-4 shadow-sm">
                     <div class="mb-3">
@@ -30,7 +30,7 @@
                     <button type="submit" class="btn btn-primary w-100">Enviar</button>
                 </form>
             </div>
-            <div class="col-md-6">
+            <div class="col-md-7">
                 <?php
                 include "api/cnx.php";
 
