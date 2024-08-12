12dd2c7f4dd99bae201ac59f79c8ae1e89f3a792
Brandon <brandonde2002@gmail.com>
2024-08-11 20:27:27 -0600

    sfdsfdsf

--- agregar.php
+++ agregar.php
@@ -117,13 +117,7 @@ if (!isset($_SESSION['logueado']) || $_SESSION['logueado'] !== true) {
 
     <!-- Incluye Bootstrap JS (Opcional) -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-     <script>
-    function cerrarSesion() {
-        fetch('local/logout.php', { method: 'POST' });
 
-    };
-
-    </script>
     
 </body>
 </html>
